f03e0db5447a8c826999ff9b315507613ff346f2
Oyku Sahin <40883071+oykusahin@users.noreply.github.com>
2026-07-26 17:05:29 +0300

    Time: 0 ms (100%), Space: 12.9 MB (95.02%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen = {}                     
+        for i, n in enumerate(nums):
+            if target - n in seen:     
+                return [seen[target - n], i]
+            seen[n] = i
+             
